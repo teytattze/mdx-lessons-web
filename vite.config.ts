f13500969618b1201bef6f2125da6866d4be0c8a
tattzetey <tattzetey@gmail.com>
2022-03-02 16:30:57 +0000

    initial commit

---
+++ vite.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import { resolve } from 'path';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+	plugins: [vue()],
+	resolve: {
+		alias: {
+			'@components': resolve(__dirname, './src/components'),
+			'@compositions': resolve(__dirname, './src/compositions'),
+			'@layouts': resolve(__dirname, './src/layouts'),
+			'@modules': resolve(__dirname, './src/modules'),
+			'@services': resolve(__dirname, './src/services'),
+			'@shared': resolve(__dirname, './src/shared'),
+			'@stores': resolve(__dirname, './src/stores'),
+		},
+	},
+});
