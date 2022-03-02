22f700e6adee9eec4cd84d842d62892165d48661
tattzetey <tattzetey@gmail.com>
2022-03-02 16:36:46 +0000

    setup fontawesome

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,34 @@
+import { library } from '@fortawesome/fontawesome-svg-core';
+import {
+	faAngleLeft,
+	faAngleRight,
+	faCartShopping,
+	faMagnifyingGlass,
+	faMinus,
+	faPlus,
+	faSchool,
+	faTimes,
+	faGlobe,
+} from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 import { createPinia } from 'pinia';
 import { createApp } from 'vue';
 import App from './App.vue';
 import 'bootstrap';
 
-createApp(App).use(createPinia()).mount('#app');
+library.add(
+	faAngleLeft,
+	faAngleRight,
+	faCartShopping,
+	faMagnifyingGlass,
+	faMinus,
+	faPlus,
+	faTimes,
+	faGlobe,
+	faSchool,
+);
+
+createApp(App)
+	.component('font-awesome-icon', FontAwesomeIcon)
+	.use(createPinia())
+	.mount('#app');
